46ce03d112168053164be7a3f8b1f572fc48d78e
eom <eomrust@users.noreply.github.com>
2022-07-16 05:33:03 +0000

    feat: control flow examples from chapter 3
    Add the control flow examples from chapter 3 of the rust book.

--- src/main.rs
+++ src/main.rs
@@ -107,18 +107,122 @@ fn numerical_operations() {
     let _remainder = 43 % 5;
 }
 
-fn main() {
-    hello_world();
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
 
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn control_flow() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // Handling Multiple Conditions with else if
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    // Using if in a let Statement
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {number}");
+
+    // Returning Values from Loops
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+
+    // Loop Labels to Disambiguate Between Multiple Loops
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    // Conditional Loops with while
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+
+    // Safer loops relative to conditional loops
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+
+    // For loops
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+}
+
+fn chapter_three_examples(){
     // 3.1
     variables_mutability();
 
-    // 3.2
+    // 3.2 Data Types
     data_types();
     numerical_operations();
     invalid_array();
 
-    // 3.2
+    // 3.3 Functions
+    print_labeled_measurement(5, 'h');
+    let x = five();
+    println!("The value of x is: {x}");
+    let x = plus_one(5);
+    println!("The value of x is: {x}");
 
+    // 3.4
+    control_flow();
+}
+
+fn main() {
+    hello_world();
     println!("{}", THREE_HOURS_IN_SECONDS);
+
+    chapter_three_examples();
 }
